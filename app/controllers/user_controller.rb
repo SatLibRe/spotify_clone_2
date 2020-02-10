7239fbc11d4a9b10e363b9e10fa909d6402fdaa6
myk245 <myk245@nyu.edu>
2020-02-10 11:26:13 -0500

    updated UsersController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,41 @@
 class UserController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.delete
+    redirect_to users_path
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name)
+  end 
+
 end

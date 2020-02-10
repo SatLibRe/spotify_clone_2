ee8619fc0a95510fcef0abea0dc37a677fc8874f
Miles Higbie <miles.higbie@gmail.com>
2020-02-10 11:23:05 -0500

    added methods for Artists

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :favorites
+  resources :artists
+  resources :genres
+  resources :songs 
+  resources :users
 end

2dbc61c3800c54d3d585ebe837f42025c0f9a31d
myk245 <myk245@nyu.edu>
2020-02-10 10:58:25 -0500

    added songs routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :artists 
   resources :favorites
   resources :users
+  resources :songs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

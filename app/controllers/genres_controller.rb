ca8920f42c41fdcde2403b22440b988a826265df
Anil Gurung <anil_grg@hotmail.com>
2020-02-10 11:27:18 -0500

    added genre methods

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,14 +1,42 @@
 class GenresController < ApplicationController
 
-    def index
-
-    end
-
-    def new
-
-    end
-
-
-
-
-end
+    def index 
+        @genres = Genres.all
+     end 
+  
+     def new 
+        @genre = Genre.new
+     end 
+  
+     def create
+        @genre = Genre.create(genre_params)
+        redirect_to genres_path
+     end 
+  
+     def show
+        @genre = Genre.find(params[:id])
+     end 
+  
+     def edit
+        @genre = Genre.find(params[:id])
+     end
+  
+     def update
+        @genre= Genre.find(params[:id])
+        @genre.update(user_params)
+        redirect_to genres_path
+     end 
+  
+     def destroy
+        @genre = Genre.find(params[:id])
+        @genre.delete
+        redirect_to genres_path
+     end 
+  
+     private
+  
+     def genre_params
+        params.require(:genre).permit(:name, :image)
+     end 
+  end
+  

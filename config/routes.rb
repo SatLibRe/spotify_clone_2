584313d45667fbac1fc6f1fb1d4d6821382534b4
Miles Higbie <miles.higbie@gmail.com>
2020-02-11 11:26:44 -0500

    added session logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,16 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/favorite_song/:song_id', to: 'favorites#create', as: 'song_favorite'
+  get '/sessions/new', to: 'sessions#new', as: 'new_session'
+  post '/sessions', to: 'sessions#create', as: 'create_session'
+  delete '/sessions', to: 'sessions#destroy', as: 'destroy_session'
+  
+
+
 
   resources :favorites
   resources :artists
   resources :genres
   resources :songs 
   resources :users
-  resources :sessions
 end

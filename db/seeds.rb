a43c5ce1aff0e0f096be0d9ce90bc7c030cb2bd9
Miles Higbie <miles.higbie@gmail.com>
2020-02-12 13:33:21 -0500

    added nav search

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,15 @@ User.destroy_all
 Song.destroy_all
 Favorite.destroy_all
 
+image_arr = [
+    "https://media.istockphoto.com/photos/group-of-cheerful-diverse-friends-in-the-park-picture-id958682698?k=6&m=958682698&s=612x612&w=0&h=pzACQnArO-EvwsEMqp2upmZ5Zmuoh3YDHhPkHdCg2os=",
+    "https://peopledotcom.files.wordpress.com/2019/11/77149507_420647148599835_4561668129413349875_n.jpg?w=1080&h=810",
+    "https://images.pexels.com/photos/1595387/pexels-photo-1595387.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+]
+
 10.times { Genre.create(name: Faker::Music.genre, image: "text") }
 
-50.times { Artist.create(name: Faker::Music.band, age: 10, bio: Faker::Lorem.paragraphs(number: 1), image: "text") }
+50.times { Artist.create(name: Faker::Music.band, age: 10, bio: Faker::Lorem.paragraphs(number: 1), image: image_arr.sample) }
 
 User.create(name: "Miles", password: "123") 
 

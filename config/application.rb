71219f74f816b298d6719d2cf3ae1e83eb6c0791
Miles Higbie <miles.higbie@gmail.com>
2020-02-13 12:01:27 -0500

    Added video to login

--- config/application.rb
+++ config/application.rb
@@ -8,9 +8,10 @@ Bundler.require(*Rails.groups)
 
 module SpotifyClone
   class Application < Rails::Application
+    config.assets.paths << "#{Rails.root}/app/assets/videos"
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.paths << "#{Rails.root}/app/assets/videos"
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
